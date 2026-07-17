9089a398b31fb669c4c42bd83d9298432c0f554c
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-17 23:48:08 +0400

    feature: index router set up

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import * as indexController from "../controllers/indexController.js"
 
 const router = Router();
 
-router.get('/', categoryController.getAllCategories)
+router.get('/', indexController.getHomepage);
+
+export default router

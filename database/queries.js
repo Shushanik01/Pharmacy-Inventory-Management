04b0d35c16e399fc7c52e33a1757a1f7770d8276
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-18 13:45:09 +0400

    bug fix

--- database/queries.js
+++ database/queries.js
@@ -22,10 +22,10 @@ export async function getManuf(){
 };
 
 export async function addMedicine(medicine){
-    const {name, description, price, stock_quality, expiration_date, category_id, manufacturer_id} = medicine
+    const {name, description, price, stock_quantity, expiration_date, category_id, manufacturer_id} = medicine
     await pool.query(
-        `INSERT INTO medicines (name, description, price, stock_quality, expiration_date, category_id, manufacturer_id)
+        `INSERT INTO medicines (name, description, price, stock_quantity, expiration_date, category_id, manufacturer_id)
         VALUES($1, $2, $3, $4, $5, $6, $7)`,
-        [name, description, price, stock_quality, expiration_date, category_id, manufacturer_id]
+        [name, description, price, stock_quantity, expiration_date, category_id, manufacturer_id]
     )
 }

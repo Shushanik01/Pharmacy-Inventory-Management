dfb4af51246e4c861e3f0253fb4d5602bc9cdd6c
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-19 10:38:29 +0400

    routes for medicine updating created

--- routers/medRouter.js
+++ routers/medRouter.js
@@ -9,6 +9,10 @@ const medRouter = Router();
 
  medRouter.post("/medicines/delete", Controller.removeMedicines)
 
- medRouter.post("/medicines/:id/delete", Controller.removeMedicine)
+ medRouter.post("/medicines/:id/delete", Controller.removeMedicine);
+
+ medRouter.get("/medicines/:id/edit", Controller.editMedicineForm);
+
+ medRouter.post("/medicines/:id/edit", Controller.updateMedicine)
 
  export default medRouter
